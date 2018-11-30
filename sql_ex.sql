695446ab3ac159d70d1b637ce6fa7a9a4a03db84
MWang93 <40410176+MWang93@users.noreply.github.com>
2018-11-29 22:51:25 -0800

    Update sql_ex.sql

--- sql_ex.sql
+++ sql_ex.sql
@@ -1,30 +1,8 @@
---Group by and Top N, Solution1: 2 CTEs Solution2: lag(), row_number()
-with last_interact(
-	select user_id, unix_timestamp as last_interaction
-	from query_one
-	group by user_id
-	having count(unix_timestamp)>=2
-	order by unix_timestamp DESC
-	limit 1
-),
-with second_last_interact(
-	select user_id, unix_timestamp as second_last_interaction
-	from query_one
-	where unix_timestamp < (select last_interact from last_ineract)
-	group by user_id
-	having count(unix_timestamp)>=1
-	order by unix_timestamp DESC
-	limit 1
-)
-select l.user_id, l.last_interaction - s.second_last_interaction as visiting_page
-from last_interact l and second_last_interact s and l.user_id = s.user_id
-limit 5
-
-
+--Recent 2 purchase time difference. Solution: lag(), row_number(), subquery
 select user_id, unix_tiemstamp - previous_time as delta_secondlastone_lastone
 from (
 		select user_id, unix_timestamp,
-			   lag(unix_timestamp) over (partition by user_id order by unix_tiemstamp) as previous_time
+			   lag(unix_timestamp,1) over (partition by user_id order by unix_tiemstamp) as previous_time
 			   row_number() over (patition by user_id order by unix_timestamp desc) as order_desc
 	    from query_one
 	 ) tmp
@@ -33,7 +11,7 @@ order by user_id
 limit 5;
 
 
---Group by and Percent
+-- Client ratio by each user. Solution: COUNT() + PARTITION BY
 with mobile_user(
 	select *, 'mobile' as client
 	from mobile_data 
@@ -57,9 +35,9 @@ from all_user
 group by user_id, client
 
 
-SELECT 100*SUM(CASE WHEN m.user_id IS null THEN 1 ELSE 0 END)/COUNT(*) as WEB_ONLY,
-	   100*SUM(CASE WHEN w.user_id IS null THEN 1 ELSE 0 END)/COUNT(*) as MOBILE_ONLY,
-	   100*SUM(CASE WHEN m.user_id IS NOT null AND w.user_id IS NOT null THEN 1 ELSE 0 END)/COUNT(*) as BOTH
+SELECT 100*SUM(CASE WHEN m.user_id IS NULL THEN 1 ELSE 0 END)/COUNT(*) as WEB_ONLY,
+	   100*SUM(CASE WHEN w.user_id IS NULL THEN 1 ELSE 0 END)/COUNT(*) as MOBILE_ONLY,
+	   100*SUM(CASE WHEN m.user_id IS NOT NULL AND w.user_id IS NOT null THEN 1 ELSE 0 END)/COUNT(*) as BOTH
 FROM (SELECT distinct user_id FROM query_two_web) w 
 	  OUTER JOIN 
 	 (SELECT distinct user_id FROM query_two_mobile) m ON m.user_id = w.user_id;
@@ -69,7 +47,7 @@ FROM (SELECT distinct user_id FROM query_two_web) w
 SELECT user_id, purchasedate
  FROM ( SELECT *, ROW_NUMBER() over(PARTITION BY user_id ORDER BY purchasedate) row_num 
 		FROM query_three ) tmp
-WHERE row_num = 10
+WHERE row_num = 3
 LIMIT 5;
 
 
@@ -86,7 +64,7 @@ GROUP BY user_id
 ORDER BY user_id
 LIMIT 5;
 
---Union and Windows_Sum() -- http://www.wagonhq.com/blog/running-totals-sql
+--Running Total, Windows_Sum() -- http://www.wagonhq.com/blog/running-totals-sql
 SELECT user_id, date,
 SUM(amount) over(PARTITION BY user_id ORDER BY date ROWS UNBOUNDED PRECEDING) as total_amount
 FROM
@@ -105,7 +83,7 @@ LIMIT 5;
 
 --Average and Mode 
 
---THe 1th and last one
+--Group by Country and Select the 1th and nth Country based on user_id COUNT
 select country, user_count
 from ( select *, row_number() over(order by user_count) as asc_num, row_number() over(order by user_count DESC) as desc_num
 	   from (
@@ -116,14 +94,13 @@ from ( select *, row_number() over(order by user_count) as asc_num, row_number()
      ) tmp
 where asc_num = 1 OR desc_num = 1
 
---Group by AND 1th and last one
+--Group by Country and Select 1th and nth user_id COUNT for each Country
 SELECT user_id, created_at, country
 FROM
 (
 	SELECT *,
 	ROW_NUMBER() OVER (PARTITION BY country ORDER BY created_at) count_asc,
-	ROW_NUMBER() OVER (PARTITION BY country ORDER BY created_at desc)
-	count_desc
+	ROW_NUMBER() OVER (PARTITION BY country ORDER BY created_at desc) count_desc
 	FROM query_six
 ) tmp
 WHERE count_asc = 1 or count_desc = 1
